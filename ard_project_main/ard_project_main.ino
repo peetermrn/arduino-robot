5a319906ea1cf7957b80bcd6d9f01af81020730a
Peeter Maran <pemara@ttu.ee>
2022-04-16 17:12:51 +0300

    robot base code setup

--- ard_project_main/ard_project_main.ino
+++ ard_project_main/ard_project_main.ino
@@ -1,9 +1,72 @@
+
+
+class Robot {
+  public:
+    float x = 0;
+    float y = 0;
+    String state; // for example scan, drive, finish
+    float leftSpeed = 0;
+    float rightSpeed = 0;
+    float laser = 0;
+
+    // set pins
+    int leftWheelPin = 1;
+    int rightWheelPin = 2;
+    int laserPin = 3;
+    
+    void setUp(){
+      //set connections for wheels, lasers etc.
+      pinMode(leftWheelPin, OUTPUT);
+      pinMode(rightWheelPin, OUTPUT);
+    };
+    
+    float setLeftSpeed(float speed){
+          leftSpeed = speed;
+    };
+    
+    float setRightSpeed(float speed){
+          rightSpeed = speed;
+    };
+
+    void sense (){
+      // refresh censors
+      // laser = analogRead(laserPin);
+    };
+
+    void plan (){
+      /* for example
+      if (laser > 0.5) {
+        setLeftSpeed(10)
+        setRightSpeed(10)
+      } else {
+        setLeftSpeed(0)
+        setRightSpeed(0)  
+      }
+      */
+    };
+
+    void act (){
+      // set voltage for left wheel
+      // analogWrite(_, _);
+      // set voltage for right wheel
+      // analogWrite(_, _);
+    };
+
+
+};
+
+Robot robot;
+  
 void setup() {
-  // put your setup code here, to run once:
+
+  robot.setUp();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // robot logic SPA
+  robot.sense();
+  robot.plan();
+  robot.act();
+  delay(50);
 }
